58c18ec74d9f180fac800d5855d9e40a9fd2a7d7
hruthika5a4 <131320755+hruthika5a4@users.noreply.github.com>
2025-09-23 19:10:15 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hyyy Sudip ');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
