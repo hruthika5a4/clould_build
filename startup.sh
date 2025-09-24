1fb960fb2dca47b76c79b23020098f993c89e8b0
hruthika5a4 <131320755+hruthika5a4@users.noreply.github.com>
2025-09-24 11:37:09 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,1 @@
-  GNU nano 7.2                                       startup.sh                                                 
-#!/bin/bash
-apt-get update
-apt-get install -y apache2
-echo "<h1>Hello from 12  MIG v123 instance $(hostname)</h1>" > /var/www/html/index.html
-systemctl restart apache2
+
