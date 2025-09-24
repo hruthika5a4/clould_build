eff5577f0a81fc058d30b18b46fac5300aace4f5
hruthika5a4 <131320755+hruthika5a4@users.noreply.github.com>
2025-09-24 10:52:23 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y apache2
-echo "<h1>Hello from MIG v3 instance $(hostname)</h1>" > /var/www/html/index.html
+echo "<h1>Hello from 12  MIG v3 instance $(hostname)</h1>" > /var/www/html/index.html
 systemctl restart apache2
