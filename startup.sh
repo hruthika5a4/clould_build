4fbd28bf53d1d04517f1414364290b7d459a1501
hruthika5a4 <131320755+hruthika5a4@users.noreply.github.com>
2025-09-25 17:11:56 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-
-# Update packages
-sudo apt-get update -y
-
-# Install Apache web server
-sudo apt-get install -y apache2
-
-# Set default HTML page with hostname
-echo "<h1>Hello from MIG instance $(hostname)</h1>" | sudo tee /var/www/html/index.html
-
-# Enable Apache to start on boot
-sudo systemctl enable apache2
-
-# Start Apache service
-sudo systemctl start apache2
+apt-get update
+apt-get install -y apache2
+echo "<h1>Hello from MIG v$(date +%s) instance $(hostname)</h1>" > /var/www/html/index.html
+systemctl restart apache2
