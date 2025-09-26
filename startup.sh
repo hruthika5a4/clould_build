90917438260f769a78891c4847fbd5645fcc3df2
hruthika5a4 <131320755+hruthika5a4@users.noreply.github.com>
2025-09-26 11:26:11 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-IMAGE="us-central1-docker.pkg.dev/singular-object-464504-a3/artifact-repo/static-web:__DEPLOY_ENV__-__VERSION__-__BUILD_ID__"
-echo "Starting container: $IMAGE"
-docker run -d -p 80:80 $IMAGE
+echo "Starting container: __IMAGE__"
+docker run -d -p 80:80 __IMAGE__
